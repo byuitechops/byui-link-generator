0c102358208a378c1c09c9c58f3acf7014f13b26
tallroids <=>
2017-12-11 14:58:28 -0700

    fixed parameter encoding

--- public/javascripts/features.js
+++ public/javascripts/features.js
@@ -1,5 +1,5 @@
     $('#link').change(function(){
       var root = "https://byui.instructure.com/courses/142/external_content/success/external_tool_dialog?return_type=lti_launch_url&url=https%3A%2F%2Flocalhost%3A1810%2Freturn%2F%3Furl="
-      var url = $('#link').val()
-      $('#return').attr("href", root + escape(encodeURIComponent(url)))
+      var url = escape($('#link').val())
+      $('#return').attr("href", root + encodeURIComponent(url))
     })
