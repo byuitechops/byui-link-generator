b5dc08559ac13a5acd8d29ba27adf029168fc696
nikhuff <nik.huff@me.com>
2017-12-12 10:35:16 -0700

    Added url string parsing logic

--- modules/parseURL.js
+++ modules/parseURL.js
@@ -5,6 +5,27 @@ function parseURL(urlString, ltiParams) {
     var email = ltiParams.lis_person_contact_email_primary
     var fullName = ltiParams.lis_person_name_full
     var username = ltiParams.custom_canvas_user_login_id;
+    
+    var element = urlString.split('{')[1].split('}')[0];
+    var newElement;
+    
+    if (element == 'fname') {
+        newElement = fname;
+    } else if (element == 'lname') {
+        newElement = lname;
+    } else if (element == 'email') {
+        newElement = email;
+    } else if (element == 'fullName') {
+        newElement = fullName;
+    } else if (element == 'username') {
+        newElement = username;
+    } else {
+        console.error('Element not recognized');
+    }
+    
+    var newUrl = urlString.replace(element, newElement);
+    
+    return newUrl;
 }
 
 module.exports = parseURL;
