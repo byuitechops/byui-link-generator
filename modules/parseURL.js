a7faaeaf1a3317ab9876021d569c7bc470f4040a
nikhuff <nik.huff@me.com>
2017-12-12 10:14:26 -0700

    added parseURL module

--- modules/parseURL.js
+++ modules/parseURL.js
@@ -1,5 +1,10 @@
 function parseURL(urlString, ltiParams) {
     console.log(urlString, ltiParams);
+    var fname = ltiParams.lis_person_name_given
+    var lname = ltiParams.lis_person_name_family
+    var email = ltiParams.lis_person_contact_email_primary
+    var fullName = ltiParams.lis_person_name_full
+    var username = ltiParams.custom_canvas_user_login_id;
 }
 
 module.exports = parseURL;
